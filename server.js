5f2db53f1c1cbeb82b0a1afa4fcd1373baaf5066
DanielKorban <d.lima@acad.ifma.edu.br>
2022-02-10 19:46:04 -0300

    desenvolvidas as interfaces home, login e cadastro

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ const path = require("path");
 const handlebars = require("express-handlebars");
 
 // Config
+    // inportar arquivos estaticos
+    app.use(express.static(path.join(__dirname, "assets")));
     // Templete engine com isto nos agora não precisamos rescrever as estruturas básicas do html
     app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
     app.set('view engine', 'handlebars');
-    // inportar arquivos estaticos
-    app.use(express.static(path.join(__dirname, "assets")));
     // Rotas
     app.get('/', function(req, res){// Rota principal
         res.render("home");
     });
 
-    app.get('/login', function(req, res){// Rota principal
+    app.get('/login', function(req, res){
         res.render("login");
+    });
+
+    app.get('/cadastro', function(req, res){
+        res.render("cadastro");
     });
 
 app.listen(5000, ()=>{
